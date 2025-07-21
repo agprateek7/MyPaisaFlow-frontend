6b20a298040cc0b4102d4bf000397a87463ec179
agprateek7 <agprateek7@gmai.com>
2025-07-21 17:37:02 +0530

    Editing

--- src/components/layouts/AuthLayout.jsx
+++ src/components/layouts/AuthLayout.jsx
@@ -6,7 +6,7 @@ const AuthLayout = ({ children }) => {
   return (
     <div className="flex">
       <div className="w-screen h-screen md:w-[60vw] px-12 pt-8 pb-12">
-        <h2 className="text-lg font-medium text-black">MyPaisaFlow</h2>
+        <h2 className="text-3xl font-medium text-black">MyPaisaFlow</h2>
         {children}
       </div>
 
